eee0fc7001638f39b91ef0e5db9db8517c3f8e46
wanderleisouza <wandi.darko@gmail.com>
2017-12-30 19:28:35 -0200

    second version, + random and time wait functions

--- odd.py
+++ odd.py
@@ -1,14 +1,19 @@
 from datetime import datetime
+import random
+import time
 
 odds = [ 1,  3,  5,  7,  9, 11, 13, 15, 17, 19,
         21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
         41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
 
-right_this_minute = datetime.today().minute
 
-if right_this_minute in odds:
-    print("This minute seems a little odd.")
-else:
-    print("Not an odd minute")
+for k in range(5):
+    right_this_minute = datetime.today().minute
+    wait_time = random.randint(1, 60)
+    print("wait for {} seconds".format(wait_time))
+    time.sleep(wait_time)
+    if right_this_minute in odds:
+        print("This minute seems a little odd.")
+    else:
+        print("Not an odd minute")
 
-print(right_this_minute)
