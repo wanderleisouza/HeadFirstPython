a7f5683f6f4cab78ab93eacfc790fdea6107db70
wanderleisouza <wandi.darko@gmail.com>
2017-12-31 21:06:05 -0200

    beer song

---
+++ beersong.py
@@ -0,0 +1,10 @@
+for i in range(99, 0, -1):
+    word = "bottles"
+    if i == 1:
+        word = "bottle"
+    print(i, word, "of beer on the wall.")
+    print(i, word, "of beer.")
+    print("take one down.")
+    print("pass it around.")
+
+print("no more bottles of beer on the wall.")
