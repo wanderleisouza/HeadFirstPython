673fa181e6d55ab8a59efb88bf47a5ce7c09c088
wanderleisouza <wandi.darko@gmail.com>
2018-01-04 11:27:19 -0200

    new version of vowels exercise

---
+++ vowels5.py
@@ -0,0 +1,11 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+word = input("type a word:").lower()
+counter = {}
+
+for c in word:
+    if c in vowels:
+        counter.setdefault(c, 0)
+        counter[c] = counter[c] + 1
+
+for k, v in sorted(counter.items()):
+    print(k, 'was found', v, 'time(s)')
